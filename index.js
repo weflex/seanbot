832166cf33a6e17b0b408f5fd8082b8768bdb9a4
Zhang Weijie <email@zhangweijie.cn>
2016-04-26 15:47:33 +0800

    rewrite with Promise and  ESlint

--- index.js
+++ index.js
@@ -1,110 +1,116 @@
-'use strict'
+'use strict';
 
 const koa = require('koa');
 const koaBody = require('koa-body');
 const GithubWebhookHandler = require('koa-github-webhook-handler');
-const github = require('./github');
+const github = require('./lib/github');
 const bumpVersion = require('semver-increment');
+const promisify = require('es6-promisify');
 
-let app = koa();
+const app = koa();
 
-let githubWebhookHandler = new GithubWebhookHandler({
+const githubWebhookHandler = new GithubWebhookHandler({
   path: '/webhook',
   secret: process.env.SEANBOT_WEBHOOK_SECRET,
 });
 
-githubWebhookHandler.on('issue_comment', (event) => {
-  const payload = event.payload;
+const getPullRequests = promisify(github.pullRequests.get.bind(github.pullRequests));
+const getContent = promisify(github.repos.getContent.bind(github.repos));
+const updateFile = promisify(github.repos.updateFile.bind(github.repos));
 
-  //judge if the comment is from pull requests
-  const pullRequest = payload['issue']['pull_request'];
-  if (!pullRequest) {
+function getMasksFromCommentBody(commentBody) {
+  let MAJOR = 0;
+  let MINOR = 0;
+  let PATCH = 0;
+  if (commentBody.match('major')) {
+    MAJOR = 1;
+  } else if (commentBody.match('minor')) {
+    MINOR = 1;
+  } else if (commentBody.match('patch')) {
+    PATCH = 1;
+  } else {
+    return null;
+  }
+  const masks = [MAJOR, MINOR, PATCH];
+  return masks;
+}
+
+function getMsgFormPrUrl(url) {
+  const list = url.split('/');
+  return {
+    user: list[4],
+    repo: list[5],
+    number: list[7],
+  };
+}
+
+function getMsgFormHtmlUrl(url) {
+  const list = url.split('/');
+  return {
+    user: list[3],
+    repo: list[4],
+    branch: list[6],
+  };
+}
+
+function issueCommentHandler(event) {
+  const payload = event.payload;
+  const pullRequests = payload.issue.pull_request;
+  if (!pullRequests) {
+    console.log('It isn\'t a pull request.');
     return;
   }
-
-  const commentBody = payload['comment']['body'].toLowerCase();
-
-  if (commentBody.match('@seanbot') && commentBody.match('bump')) {
-
-    function getMasksFromCommentBody() {
-      let MAJOR = 0;
-      let MINOR = 0;
-      let PATCH = 0;
-      if (commentBody.match('major')) {
-        MAJOR = 1;
-      } else if (commentBody.match('minor')) {
-        MINOR = 1;
-      } else if (commentBody.match('patch')) {
-        PATCH = 1;
-      } else {
-        return;
-      }
-      const masks = [MAJOR, MINOR, PATCH];
-      return masks;
-    }
-
-    function getMsgFormPrUrl() {
-      const list = pullRequest['url'].split('/');
-      return {
-        user: list[4],
-        repo: list[5],
-        number: list[7]
-      };
-    }
-
-    const masks = getMasksFromCommentBody();
-    if (!masks) {
-      return;
-    }
-
-    github.pullRequests.get(getMsgFormPrUrl(), (err, res) => {
-      if (err) {
-        console.error(err && err.stack);
-        return;
-      }
-      const data = {
-        user: res['head']['repo']['owner']['login'],
-        repo: res['head']['repo']['name'],
-        path: 'package.json',
-        ref: res['head']['ref']
-      };
-
-      github.repos.getContent(data, (err, res) => {
-        if (err) {
-          console.error(err && err.stack);
-          return;
-        }
-
-        const content = res['content'];
-        const sha = res['sha'];
-        let _package = JSON.parse(new Buffer(content, 'base64').toString());
-        _package.version = bumpVersion(masks, _package.version);
-
-        github.repos.updateFile({
-          user: data['user'],
-          repo: data['repo'],
-          path: 'package.json',
-          branch: data['ref'],
-          message: 'bump version to ' + _package.version,
-          content: new Buffer(JSON.stringify(_package, null, '  ')).toString('base64'),
-          sha: res.sha
-        }, (err, res) => {
-          if (err) {
-            console.log(err && err.stack);
-            return;
-          }
-          console.log(res);
-        });
-      });
-    });
+  const commentBody = payload.comment.body.toLowerCase();
+  const masks = getMasksFromCommentBody(commentBody);
+  if (!commentBody.match('@seanbot') || !commentBody.match('bump' || !masks)) {
+    console.log(commentBody, 'It\'s not a bump action.');
+    return;
   }
-});
+  getPullRequests(getMsgFormPrUrl(pullRequests.url))
+  .then((res) => {
+    const baseMsg = {
+      user: res.base.repo.owner.login,
+      repo: res.base.repo.name,
+      path: 'package.json',
+      ref: res.base.ref,
+    };
+    const headMsg = {
+      user: res.head.repo.owner.login,
+      repo: res.head.repo.name,
+      path: 'package.json',
+      ref: res.head.ref,
+    };
+    return Promise.all([getContent(baseMsg), getContent(headMsg)]);
+  })
+  .then((results) => {
+    const baseRes = results[0];
+    const headRes = results[1];
+    const basePackageJson = JSON.parse(new Buffer(baseRes.content, 'base64').toString());
+    const headPackageJson = JSON.parse(new Buffer(headRes.content, 'base64').toString());
+    headPackageJson.version = bumpVersion(masks, basePackageJson.version);
+    const msg = getMsgFormHtmlUrl(headRes.html_url);
+    msg.path = 'package.json';
+    msg.message = `version: ${basePackageJson.version} bump to :version ${headPackageJson.version}`;
+    msg.content = new Buffer(JSON.stringify(headPackageJson, null, '  ')).toString('base64');
+    msg.sha = headRes.sha;
+    return updateFile(msg);
+  })
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err.message && err.stack));
+}
 
+githubWebhookHandler.on('issue_comment', issueCommentHandler);
 
 app.use(koaBody({
   formidable: {
-    uploadDir: __dirname}
-  }
-));
+    uploadDir: __dirname,
+  },
+}));
+
 app.use(githubWebhookHandler.middleware());
 app.listen((process.env.PORT || 4444));
+
+exports.getMasksFromCommentBody = getMasksFromCommentBody;
+exports.getMsgFormPrUrl = getMsgFormPrUrl;
+exports.getMsgFormHtmlUrl = getMsgFormHtmlUrl;
+exports.issueCommentHandler = issueCommentHandler;

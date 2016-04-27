1e7c654d937cc28958d4edf2165dba42d40e1681
Zhang Weijie <email@zhangweijie.cn>
2016-04-27 13:23:37 +0800

    add test case

--- test/test.js
+++ test/test.js
@@ -1,4 +1,23 @@
 'use strict';
 
 const tap = require('tap');
-tap.pass('this is fine');
+const getMasksFromCommentBody = require('../lib/function').getMasksFromCommentBody;
+const getPrMsgFormUrl = require('../lib/function').getPrMsgFormUrl;
+const getBranchMsgFormHtmlUrl = require('../lib/function').getBranchMsgFormHtmlUrl;
+
+tap.equal(getMasksFromCommentBody('@seanbot bump'), null);
+tap.same(getMasksFromCommentBody('@seanbot bump major'), [1, 0, 0]);
+tap.same(getMasksFromCommentBody('@seanbot bump minor'), [0, 1, 0]);
+tap.same(getMasksFromCommentBody('@seanbot bump patch'), [0, 0, 1]);
+
+tap.same(getPrMsgFormUrl('https://api.github.com/repos/weflex/seanbot/pulls/1'), {
+  user: 'weflex',
+  repo: 'seanbot',
+  number: '1',
+});
+
+tap.same(getBranchMsgFormHtmlUrl('https://github.com/weflex/seanbot/blob/master/package.json'), {
+  user: 'weflex',
+  repo: 'seanbot',
+  branch: 'master',
+});

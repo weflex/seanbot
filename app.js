bf641a9ac59aba2bb7ef27c66ef69a1dd98d824f
Zhang Weijie <email@zhangweijie.cn>
2016-04-27 12:56:52 +0800

    move some function in lib/function rename lib/index to issueCommentHandler

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const koa = require('koa');
 const koaBody = require('koa-body');
 const GithubWebhookHandler = require('koa-github-webhook-handler');
-const issueCommentHandler = require('./lib/index.js').issueCommentHandler;
+const issueCommentHandler = require('./lib/issueCommentHandler');
 const app = koa();
 
 const githubWebhookHandler = new GithubWebhookHandler({

2c21b85e38ef94dd73faf82f60a1f50a742da4e7
Zhang Weijie <email@zhangweijie.cn>
2016-04-27 12:06:21 +0800

    move some function to lib

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const koa = require('koa');
+const koaBody = require('koa-body');
+const GithubWebhookHandler = require('koa-github-webhook-handler');
+const issueCommentHandler = require('./lib/index.js').issueCommentHandler;
+const app = koa();
+
+const githubWebhookHandler = new GithubWebhookHandler({
+  path: '/webhook',
+  secret: process.env.SEANBOT_WEBHOOK_SECRET,
+});
+
+githubWebhookHandler.on('issue_comment', issueCommentHandler);
+
+app.use(koaBody({
+  formidable: {
+    uploadDir: __dirname,
+  },
+}));
+
+app.use(githubWebhookHandler.middleware());
+app.listen((process.env.PORT || 4444));

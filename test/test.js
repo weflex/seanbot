ac7214ddf53b136d8f23492763ebe7828eb3cb43
Zhang Weijie <email@zhangweijie.cn>
2016-04-28 16:04:42 +0800

    add feature fuzzy matching

--- test/test.js
+++ test/test.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const test = require('tape');
-const getMasksFromCommentBody = require('../lib/utils').getMasksFromCommentBody;
+const parseCommentBody = require('../lib/utils').parseCommentBody;
 const getPrMsgFormUrl = require('../lib/utils').getPrMsgFormUrl;
 const getBranchMsgFormHtmlUrl = require('../lib/utils').getBranchMsgFormHtmlUrl;
 
-test('getMasksFromCommentBody function', function(t) {
-  t.equal(getMasksFromCommentBody('@seanbot bump'), null);
-  t.deepEqual(getMasksFromCommentBody('@seanbot bump major'), [1, 0, 0]);
-  t.deepEqual(getMasksFromCommentBody('@seanbot bump minor'), [0, 1, 0]);
-  t.deepEqual(getMasksFromCommentBody('@seanbot bump patch'), [0, 0, 1]);
+test('utils.parseCommentBody', function(t) {
+  t.equal(parseCommentBody('@seanbot bump'), null);
+  t.deepEqual(parseCommentBody('@seanbot bump major'), [1, 0, 0]);
+  t.deepEqual(parseCommentBody('@seanbot bump minor'), [0, 1, 0]);
+  t.deepEqual(parseCommentBody('@seanbot bump patch'), [0, 0, 1]);
+  t.deepEqual(parseCommentBody('@sanbot bmp msjor'), [1, 0, 0]);
+  t.deepEqual(parseCommentBody('@senbot bup mimor'), [0, 1, 0]);
+  t.deepEqual(parseCommentBody('@seanot dump patxh'), [0, 0, 1]);
   t.end();
 });
 
